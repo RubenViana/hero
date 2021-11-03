eae56b45e799ea8bd8f91092b7f36e27fa52180e
ruben viana <up202005108@up.pt>
2021-11-03 12:07:44 +0000

    parte 7 done

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,8 +9,13 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
+import static com.googlecode.lanterna.input.KeyType.ArrowUp;
+
 public class Game {
-    public Screen screen;
+
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -27,12 +34,33 @@ public class Game {
         }
     }
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                screen.close();
+            }
+            else if (key.getKeyType() == KeyType.EOF){
+                break;
+            }
+        }
+
+    }
+
+    private void processKey(KeyStroke key){
+        switch (key.getKeyType()){
+            case ArrowUp : y --; break;
+            case ArrowDown : y ++; break;
+            case ArrowLeft: x--; break;
+            case ArrowRight: x++; break;
+        }
     }
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')
                 [0]);
         screen.refresh();
     }
