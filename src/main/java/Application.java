50643077ed4d3103441c4e3c6b0532db725ed6e0
ruben viana <up202005108@up.pt>
2021-11-03 10:29:11 +0000

    using lanterna done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,7 +10,12 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory()
+                    .setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            
             Screen screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
